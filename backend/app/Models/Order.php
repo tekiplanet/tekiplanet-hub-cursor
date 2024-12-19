56941ef9309775e83744625fe4526b848a81d9ec
tekiplanet <officialtekiplanet@gmail.com>
2024-12-19 15:28:15 +0100

    STORE FEATURES STARTED - working on checkout page - still debugging purchase

--- backend/app/Models/Order.php
+++ backend/app/Models/Order.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
 
 class Order extends Model
 {
+
+    use HasUuids;
+
     protected $fillable = [
         'user_id',
         'shipping_address_id',
